05652c2a7844a0b73f50eb889054f29826f3c6a9
ShawnYant <yanqiyu2002@gmail.com>
2022-05-18 17:05:33 -0400

    N/A

--- Customer/models.py
+++ Customer/models.py
@@ -22,7 +22,8 @@ class update(models.Model):
     email = models.EmailField(unique= True,default='123@123.com') # EMAIL
     address = models.CharField(max_length=100,default='123')   #customer address
     phoneNo = models.CharField(max_length=10,default='123')  #customer phone number
-    status = models.IntegerField(default=1)    #Status:1:Available/2:Disabled/6:System Administrator/9:Delete
+    birthdate = models.DateTimeField(max_length=10,default='123')  #customer phone number
+    status = models.IntegerField(default=1)    #Status:pending for next sprint
 
     def toDict(self):
         return {'nickname':self.nickname,'email':self.email,'address':self.address,'phoneNo':self.phoneNo,'status':self.status,'update_at':self.update_at.strftime('%Y-%m-%d %H:%M:%S'),} 
