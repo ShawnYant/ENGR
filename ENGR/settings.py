d4260f2167c31c938084a2de89ffa32d62baf097
ShawnYant <yanqiyu2002@gmail.com>
2022-05-23 10:16:24 -0400

    checking

--- ENGR/settings.py
+++ ENGR/settings.py
@@ -81,8 +81,14 @@ WSGI_APPLICATION = 'ENGR.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'database.amazonaws.com',
+        'PORT': '3306',
+        'ENGINE': 'django.db.backends.mysql'
     }
 }
 
